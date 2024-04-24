b4a82399498ac0fe4387a8bfb768ae1f9eadd9fc
federico <carlos.federico005@gmail.com>
2024-04-24 07:02:11 -0400

    feat: add successful submition modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Stack } from "@mui/material";
 import { Footer } from "./components/blocks/Footer";
 import { Navbar } from "./components/blocks/Navbar";
@@ -7,11 +8,15 @@ import { WorkExperienceBlock } from "./components/blocks/workExperience/WorkExpe
 import { LanguagesBlock } from "./components/blocks/languages/LanguagesBlock";
 import { AttachResumeBlock } from "./components/blocks/AttachResumeBlock";
 import { SubmitBlock } from "./components/blocks/SubmitBlock";
+import { SuccessSubmition } from "./components/popup/SuccessSubmition";
 import "./App.css";
 
 function App() {
+  const [submitted, setSubmitted] = useState<boolean>(false);
+
   return (
     <Stack maxWidth={339}>
+      <SuccessSubmition open={submitted} onAccept={() => setSubmitted(false)} />
       <Navbar />
       <WorkWithUs />
       <Stack px={1}>
@@ -19,7 +24,7 @@ function App() {
         <WorkExperienceBlock />
         <LanguagesBlock />
         <AttachResumeBlock />
-        <SubmitBlock />
+        <SubmitBlock onSubmit={() => setSubmitted(true)} />
       </Stack>
       <Footer />
     </Stack>
---
+++ src/components/popup/SuccessSubmition.tsx
@@ -0,0 +1,53 @@
+import { Modal, Paper, Stack, Typography } from "@mui/material";
+import { FC } from "react";
+import CheckIcon from "@mui/icons-material/Check";
+import { AcceptBtn } from "../buttons/AcceptBtn";
+
+interface SuccessSubmitionProps {
+  open: boolean;
+  onAccept: () => void;
+}
+
+export const SuccessSubmition: FC<SuccessSubmitionProps> = ({
+  open,
+  onAccept,
+}) => {
+  return (
+    <Modal
+      open={open}
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Paper
+        sx={{
+          borderRadius: 2,
+          py: 4,
+          px: 5,
+          maxWidth: "339px",
+        }}
+      >
+        <Stack gap={4}>
+          <Stack gap={2}>
+            <CheckIcon
+              sx={{
+                mx: "auto",
+                width: "92px",
+                height: "92px",
+                borderRadius: "50% 50%",
+                backgroundColor: "#D9F4EC",
+              }}
+              htmlColor="#0EDFA1"
+            />
+            <Typography variant="h1" align="center">
+              Tu postulación fue enviada con éxito
+            </Typography>
+          </Stack>
+          <AcceptBtn onClick={onAccept} />
+        </Stack>
+      </Paper>
+    </Modal>
+  );
+};
