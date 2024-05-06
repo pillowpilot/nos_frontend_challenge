59e0f0e21423cef3badf96b31b54b7b3e025080b
federico <carlos.federico005@gmail.com>
2024-05-06 13:14:37 -0400

    feat: add file input functionality to resume btn

--- src/components/buttons/ResumeBtn.tsx
+++ src/components/buttons/ResumeBtn.tsx
@@ -1,24 +1,48 @@
 import { FC } from "react";
-import { Button } from "@mui/material";
-import AttachFileIcon from '@mui/icons-material/AttachFile';
+import { Button, styled } from "@mui/material";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import { Controller, useFormContext } from "react-hook-form";
+
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
+});
 
 export const ResumeBtn: FC = () => {
+  const { control } = useFormContext();
   return (
-    <Button
-      variant="contained"
-      color="addWorkBtn"
-      disableElevation
-      startIcon={<AttachFileIcon htmlColor="#1A3082" />}
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        px: 2,
-        py: 1.5,
-        borderRadius: "8px",
-        textTransform: "none",
+    <Controller
+      name="cvfile"
+      control={control}
+      render={({ field }) => {
+        return (
+          <Button
+            variant="contained"
+            color="addWorkBtn"
+            disableElevation
+            component="label"
+            startIcon={<AttachFileIcon htmlColor="#1A3082" />}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              px: 2,
+              py: 1.5,
+              borderRadius: "8px",
+              textTransform: "none",
+            }}
+          >
+            Subir CV
+            <VisuallyHiddenInput {...field} type="file" />
+          </Button>
+        );
       }}
-    >
-      Subir CV
-    </Button>
+    />
   );
 };
