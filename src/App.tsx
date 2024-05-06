fb619cbc4e07049898fe1b542ed66b1498f82023
federico <carlos.federico005@gmail.com>
2024-05-06 12:59:02 -0400

    feat: format all form data and print it

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useForm, FormProvider, SubmitHandler } from "react-hook-form";
 import { Stack } from "@mui/material";
 import { Footer } from "./components/blocks/Footer";
 import { Navbar } from "./components/blocks/Navbar";
@@ -11,23 +12,91 @@ import { SubmitBlock } from "./components/blocks/SubmitBlock";
 import { SuccessSubmition } from "./components/popup/SuccessSubmition";
 import "./App.css";
 
+type WorkExperienceFields = {
+  companyName: string;
+  position: string;
+  from: string;
+  to: string;
+};
+
+type LanguageFields = {
+  language: string;
+  level: string;
+};
+
+type FormFields = {
+  fullname: string;
+  birthday: string;
+  cellphone: string;
+  workExperience: WorkExperienceFields[];
+  language: LanguageFields[];
+};
+
+const useFormatter = () => {
+  const formatPersonalData = (data: FormFields): string => {
+    let result = "";
+    result += `Nombre Completo: ${data.fullname}.\n`;
+    result += `Fecha de Cumpleaños: ${data.birthday}.\n`;
+    result += `Número de Celular: ${data.cellphone}\n`;
+    return result;
+  };
+
+  const formatWorkExperience = (data: FormFields): string => {
+    let result = "";
+    data.workExperience.forEach((experience) => {
+      result += `Compañia: ${experience.companyName}.\n`;
+      result += `Puesto: ${experience.position}.\n`;
+      result += `Desde: .\n`;
+      result += `Hasta: .\n`;
+    });
+    return result;
+  };
+
+  const formatLanguages = (data: FormFields): string => {
+    let result = "";
+    data.language.forEach((language) => {
+      result += `Idioma: ${language.language}.\n`;
+      result += `Nivel: ${language.level}.\n`;
+    });
+    return result;
+  };
+
+  return { formatPersonalData, formatWorkExperience, formatLanguages };
+};
+
 function App() {
+  const methods = useForm<FormFields>();
   const [submitted, setSubmitted] = useState<boolean>(false);
+  const { formatPersonalData, formatWorkExperience, formatLanguages } =
+    useFormatter();
+
+  const onSubmit: SubmitHandler<FormFields> = (data) => {
+    console.log(data);
+    console.log(formatPersonalData(data));
+    console.log(formatWorkExperience(data));
+    console.log(formatLanguages(data));
+    setSubmitted(true);
+  };
 
   return (
-    <Stack maxWidth={376}>
-      <SuccessSubmition open={submitted} onAccept={() => setSubmitted(false)} />
-      <Navbar />
-      <WorkWithUs />
-      <Stack px={1}>
-        <PersonalDataBlock />
-        <WorkExperienceBlock />
-        <LanguagesBlock />
-        <AttachResumeBlock />
-        <SubmitBlock onSubmit={() => setSubmitted(true)} />
+    <FormProvider {...methods}>
+      <Stack maxWidth={376}>
+        <SuccessSubmition
+          open={submitted}
+          onAccept={() => setSubmitted(false)}
+        />
+        <Navbar />
+        <WorkWithUs />
+        <Stack px={1}>
+          <PersonalDataBlock />
+          <WorkExperienceBlock />
+          <LanguagesBlock />
+          <AttachResumeBlock />
+          <SubmitBlock onSubmit={methods.handleSubmit(onSubmit)} />
+        </Stack>
+        <Footer />
       </Stack>
-      <Footer />
-    </Stack>
+    </FormProvider>
   );
 }
 
