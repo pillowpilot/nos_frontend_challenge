23ba18b5d3c8850a6199d0d9bbfccf650490d0a9
federico <carlos.federico005@gmail.com>
2024-05-06 11:37:49 -0400

    refactor: add controller to custom select

--- src/components/selects/CustomSelect.tsx
+++ src/components/selects/CustomSelect.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { Box, MenuItem, Select, Typography } from "@mui/material";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { Controller, useFormContext } from "react-hook-form";
 
 interface LanguageOption {
   value: string;
@@ -9,15 +10,19 @@ interface LanguageOption {
 
 interface LanguageSelectProps {
   label: string;
+  formKey: string;
   placeholder: string;
   options: LanguageOption[];
 }
 
 export const CustomSelect: FC<LanguageSelectProps> = ({
   label,
+  formKey,
   placeholder,
   options,
 }) => {
+  const { control } = useFormContext();
+
   const findLabel = (value: string): string | undefined => {
     const option: LanguageOption | undefined = options.find(
       (option) => option.value === value
@@ -61,47 +66,57 @@ export const CustomSelect: FC<LanguageSelectProps> = ({
       <Typography variant="h3" align="left">
         {label}
       </Typography>
-      <Select
-        fullWidth
-        defaultValue=""
-        displayEmpty
-        {...disableDropdownPadding}
-        IconComponent={KeyboardArrowDownIcon}
-        sx={{
-          ...setBorderStyles,
-          ...setIconColor,
-        }}
-        renderValue={(value) => {
-          return value !== "" ? (
-            <Typography variant="body2">{findLabel(value)}</Typography>
-          ) : (
-            <Typography
-              variant="body2"
+      <Controller
+        name={formKey}
+        control={control}
+        render={({ field }) => {
+          if(field.value === undefined)
+            field.value = "";
+          return (
+            <Select
+              fullWidth
+              displayEmpty
+              {...field}
+              {...disableDropdownPadding}
+              IconComponent={KeyboardArrowDownIcon}
               sx={{
-                color: "#C1C1C1",
+                ...setBorderStyles,
+                ...setIconColor,
+              }}
+              renderValue={(value) => {
+                return value? (
+                  <Typography variant="body2">{findLabel(value)}</Typography>
+                ) : (
+                  <Typography
+                    variant="body2"
+                    sx={{
+                      color: "#C1C1C1",
+                    }}
+                  >
+                    {placeholder}
+                  </Typography>
+                );
               }}
             >
-              {placeholder}
-            </Typography>
+              {options.map((option: LanguageOption) => (
+                <MenuItem
+                  key={option.value}
+                  value={option.value}
+                  sx={{
+                    py: "17px",
+                    px: "20px",
+                    borderColor: "#EEEEEE",
+                    borderBottomStyle: "solid",
+                    borderWidth: "1px",
+                  }}
+                >
+                  <Typography variant="body2">{option.label}</Typography>
+                </MenuItem>
+              ))}
+            </Select>
           );
         }}
-      >
-        {options.map((option: LanguageOption) => (
-          <MenuItem
-            key={option.value}
-            value={option.value}
-            sx={{
-              py: "17px",
-              px: "20px",
-              borderColor: "#EEEEEE",
-              borderBottomStyle: "solid",
-              borderWidth: "1px",
-            }}
-          >
-            <Typography variant="body2">{option.label}</Typography>
-          </MenuItem>
-        ))}
-      </Select>
+      />
     </Box>
   );
 };
